8f5967d7c2f06eb522a6cb7d8b181200dcf13115
Ivan Arriola <iarriolar@gmail.com>
2025-10-15 19:58:00 -0600

    feat(configx): Add new JWT configuration properties

--- configx/helper/loader.go
+++ configx/helper/loader.go
@@ -1,11 +1,24 @@
 package helper
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 // Load is a helper to read an environment variable or return a default value.
 func Load(envVar string, defaultVal string) string {
 	if value, exists := os.LookupEnv(envVar); exists {
 		return value
+	}
+	return defaultVal
+}
+
+// Load is a helper to read an environment variable or return a default value.
+func LoadInt(envVar string, defaultVal int) int {
+	if value, exists := os.LookupEnv(envVar); exists {
+		if intValue, err := strconv.Atoi(value); err == nil {
+			return intValue
+		}
 	}
 	return defaultVal
 }
